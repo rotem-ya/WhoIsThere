0cba6ee97c4da1870e89835fa55fb6970da3ff25
rotem-ya <67547775+rotem-ya@users.noreply.github.com>
2026-05-03 01:26:27 +0300

    Add letter card inventory to players

--- lib/models/player_model.dart
+++ lib/models/player_model.dart
@@ -5,6 +5,7 @@ class PlayerModel extends Equatable {
   final String name;
   final String? photoUrl;
   final int score;
+  final int letterCards;
   final bool isEliminated;
   final bool isHost;
   final bool isBot;
@@ -14,6 +15,7 @@ class PlayerModel extends Equatable {
     required this.name,
     this.photoUrl,
     required this.score,
+    this.letterCards = 0,
     this.isEliminated = false,
     this.isHost = false,
     this.isBot = false,
@@ -25,6 +27,7 @@ class PlayerModel extends Equatable {
       name: data['name'] ?? '',
       photoUrl: data['photoUrl'],
       score: data['score'] ?? 0,
+      letterCards: data['letterCards'] ?? 0,
       isEliminated: data['isEliminated'] ?? false,
       isHost: data['isHost'] ?? false,
       isBot: data['isBot'] ?? false,
@@ -35,6 +38,7 @@ class PlayerModel extends Equatable {
         'name': name,
         'photoUrl': photoUrl,
         'score': score,
+        'letterCards': letterCards,
         'isEliminated': isEliminated,
         'isHost': isHost,
         'isBot': isBot,
@@ -44,6 +48,7 @@ class PlayerModel extends Equatable {
     String? name,
     String? photoUrl,
     int? score,
+    int? letterCards,
     bool? isEliminated,
     bool? isHost,
     bool? isBot,
@@ -53,11 +58,20 @@ class PlayerModel extends Equatable {
         name: name ?? this.name,
         photoUrl: photoUrl ?? this.photoUrl,
         score: score ?? this.score,
+        letterCards: letterCards ?? this.letterCards,
         isEliminated: isEliminated ?? this.isEliminated,
         isHost: isHost ?? this.isHost,
         isBot: isBot ?? this.isBot,
       );
 
   @override
-  List<Object?> get props => [id, name, score, isEliminated, isHost, isBot];
+  List<Object?> get props => [
+        id,
+        name,
+        score,
+        letterCards,
+        isEliminated,
+        isHost,
+        isBot,
+      ];
 }
