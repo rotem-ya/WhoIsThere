a365ce9aed9c97636b22168e8c438b6fd1cdddac
rotem-ya <67547775+rotem-ya@users.noreply.github.com>
2026-05-06 08:24:36 +0300

    style: add 3d vault tile interactions

--- lib/widgets/game/vault_gem_tile.dart
+++ lib/widgets/game/vault_gem_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class VaultGemTile extends StatelessWidget {
+class VaultGemTile extends StatefulWidget {
   final bool isRevealed;
   final Widget child;
 
@@ -11,10 +11,46 @@ class VaultGemTile extends StatelessWidget {
   });
 
   @override
+  State<VaultGemTile> createState() => _VaultGemTileState();
+}
+
+class _VaultGemTileState extends State<VaultGemTile> {
+  bool _isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(milliseconds: 300),
-      child: isRevealed ? _buildRevealed() : _buildHidden(),
+    final scale = _isPressed ? 0.94 : 1.0;
+
+    return Listener(
+      onPointerDown: (_) => setState(() => _isPressed = true),
+      onPointerUp: (_) => setState(() => _isPressed = false),
+      onPointerCancel: (_) => setState(() => _isPressed = false),
+      child: AnimatedScale(
+        scale: scale,
+        duration: const Duration(milliseconds: 150),
+        curve: Curves.easeOut,
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 350),
+          transitionBuilder: (child, animation) {
+            final scaleAnimation = TweenSequence<double>([
+              TweenSequenceItem(
+                tween: Tween(begin: 0.96, end: 1.06).chain(CurveTween(curve: Curves.easeOut)),
+                weight: 55,
+              ),
+              TweenSequenceItem(
+                tween: Tween(begin: 1.06, end: 1.0).chain(CurveTween(curve: Curves.easeInOut)),
+                weight: 45,
+              ),
+            ]).animate(animation);
+
+            return ScaleTransition(
+              scale: scaleAnimation,
+              child: FadeTransition(opacity: animation, child: child),
+            );
+          },
+          child: widget.isRevealed ? _buildRevealed() : _buildHidden(),
+        ),
+      ),
     );
   }
 
@@ -23,18 +59,55 @@ class VaultGemTile extends StatelessWidget {
       key: const ValueKey('vault_hidden'),
       decoration: BoxDecoration(
         color: const Color(0xFF050A14),
-        borderRadius: BorderRadius.circular(4),
-        border: Border.all(
-          color: const Color(0xFFC5A021),
-          width: 1.5,
-        ),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: const Color(0xFFC5A021), width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.58),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+          BoxShadow(
+            color: const Color(0xFFC5A021).withOpacity(0.18),
+            blurRadius: 0,
+            offset: const Offset(0, -2),
+            spreadRadius: -1,
+          ),
+        ],
       ),
-      child: const Center(
-        child: Icon(
-          Icons.question_mark,
-          color: Color(0xFFD4AF37),
-          size: 26,
-        ),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.white.withOpacity(0.06),
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.10),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Center(
+            child: Icon(
+              Icons.help_outline_rounded,
+              color: Color(0xFFD4AF37),
+              size: 32,
+              shadows: [
+                Shadow(
+                  color: Colors.black45,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -43,15 +116,42 @@ class VaultGemTile extends StatelessWidget {
     return Container(
       key: const ValueKey('vault_revealed'),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
+        borderRadius: BorderRadius.circular(8),
         border: Border.all(
-          color: const Color(0xFF87CEEB),
-          width: 1.5,
+          color: const Color(0xFF87CEEB).withOpacity(0.82),
+          width: 2,
         ),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xFF87CEEB).withOpacity(0.36),
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
       ),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(4),
-        child: child,
+        borderRadius: BorderRadius.circular(6),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            widget.child,
+            Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.white.withOpacity(0.10),
+                      Colors.transparent,
+                      Colors.black.withOpacity(0.05),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
