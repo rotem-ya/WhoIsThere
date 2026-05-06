2efe936b446834f693e139200e92c71f1f87ed41
rotem-ya <67547775+rotem-ya@users.noreply.github.com>
2026-05-07 01:10:37 +0300

    refactor vault tile renderer

--- lib/widgets/game/vault_gem_tile.dart
+++ lib/widgets/game/vault_gem_tile.dart
@@ -1,124 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-class VaultGemTile extends StatefulWidget {
+class VaultGemTile extends StatelessWidget {
   final bool isRevealed;
+  final bool isFocused;
   final Widget child;
 
   const VaultGemTile({
     super.key,
     required this.isRevealed,
     required this.child,
+    this.isFocused = false,
   });
 
-  @override
-  State<VaultGemTile> createState() => _VaultGemTileState();
-}
-
-class _VaultGemTileState extends State<VaultGemTile> {
-  bool _isPressed = false;
-
-  @override
-  void didUpdateWidget(covariant VaultGemTile oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.isRevealed && !oldWidget.isRevealed) {
-      HapticFeedback.mediumImpact();
-    }
-  }
+  static const Color _gold = Color(0xFFD4AF37);
+  static const Color _navyBlack = Color(0xFF050A14);
+  static const Color _cyan = Color(0xFF87CEEB);
 
   @override
   Widget build(BuildContext context) {
-    final scale = _isPressed ? 0.94 : 1.0;
-
-    return Listener(
-      onPointerDown: (_) {
-        if (!widget.isRevealed && !_isPressed) {
-          HapticFeedback.lightImpact();
-          setState(() => _isPressed = true);
-        }
-      },
-      onPointerUp: (_) => setState(() => _isPressed = false),
-      onPointerCancel: (_) => setState(() => _isPressed = false),
-      child: AnimatedScale(
-        scale: scale,
-        duration: const Duration(milliseconds: 150),
-        curve: Curves.easeOut,
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 350),
-          transitionBuilder: (child, animation) {
-            final scaleAnimation = TweenSequence<double>([
-              TweenSequenceItem(
-                tween: Tween(begin: 0.96, end: 1.06).chain(CurveTween(curve: Curves.easeOut)),
-                weight: 55,
-              ),
-              TweenSequenceItem(
-                tween: Tween(begin: 1.06, end: 1.0).chain(CurveTween(curve: Curves.easeInOut)),
-                weight: 45,
-              ),
-            ]).animate(animation);
-
-            return ScaleTransition(
-              scale: scaleAnimation,
-              child: FadeTransition(opacity: animation, child: child),
-            );
-          },
-          child: widget.isRevealed ? _buildRevealed() : _buildHidden(),
-        ),
-      ),
-    );
+    return isRevealed ? _revealed() : _hidden();
   }
 
-  Widget _buildHidden() {
+  Widget _hidden() {
     return Container(
       key: const ValueKey('vault_hidden'),
       decoration: BoxDecoration(
-        color: const Color(0xFF050A14),
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: const Color(0xFFC5A021), width: 1.5),
+        color: _navyBlack,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: isFocused ? _cyan : _gold.withOpacity(0.30),
+          width: isFocused ? 2.5 : 1.0,
+        ),
         boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.58),
-            blurRadius: 8,
-            offset: const Offset(0, 4),
-          ),
-          BoxShadow(
-            color: const Color(0xFFC5A021).withOpacity(0.18),
-            blurRadius: 0,
-            offset: const Offset(0, -2),
-            spreadRadius: -1,
-          ),
+          BoxShadow(color: Colors.black.withOpacity(0.62), blurRadius: 9, offset: const Offset(0, 5)),
+          if (isFocused) BoxShadow(color: _cyan.withOpacity(0.30), blurRadius: 12, spreadRadius: 1),
         ],
       ),
       child: Stack(
+        fit: StackFit.expand,
         children: [
-          Positioned.fill(
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.white.withOpacity(0.06),
-                    Colors.transparent,
-                    Colors.black.withOpacity(0.10),
-                  ],
-                ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.white.withOpacity(0.055), Colors.transparent, Colors.black.withOpacity(0.18)],
               ),
             ),
           ),
-          const Center(
+          Center(
             child: Icon(
-              Icons.help_outline_rounded,
-              color: Color(0xFFD4AF37),
-              size: 32,
-              shadows: [
-                Shadow(
-                  color: Colors.black45,
-                  blurRadius: 4,
-                  offset: Offset(0, 2),
-                ),
-              ],
+              Icons.lock_outline_rounded,
+              color: _gold.withOpacity(isFocused ? 0.44 : 0.24),
+              size: 28,
             ),
           ),
         ],
@@ -126,46 +61,16 @@ class _VaultGemTileState extends State<VaultGemTile> {
     );
   }
 
-  Widget _buildRevealed() {
+  Widget _revealed() {
     return Container(
       key: const ValueKey('vault_revealed'),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: const Color(0xFF87CEEB).withOpacity(0.82),
-          width: 2,
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0xFF87CEEB).withOpacity(0.36),
-            blurRadius: 10,
-            spreadRadius: 1,
-          ),
-        ],
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: _gold.withOpacity(0.80), width: 1.0),
       ),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(6),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            widget.child,
-            Positioned.fill(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Colors.white.withOpacity(0.10),
-                      Colors.transparent,
-                      Colors.black.withOpacity(0.05),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+        borderRadius: BorderRadius.circular(11),
+        child: child,
       ),
     );
   }
