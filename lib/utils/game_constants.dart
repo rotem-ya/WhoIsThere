330eb6d90882d2e304ad58cd4d7496e778d617f6
rotem-ya <67547775+rotem-ya@users.noreply.github.com>
2026-05-06 23:52:03 +0300

    chore: add shared game constants

---
+++ lib/utils/game_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kGold = Color(0xFFD4AF37);
+const kNavy = Color(0xFF07101F);
+const kNavyBlack = Color(0xFF050A14);
+const kCyan = Color(0xFF87CEEB);
+const kTapScale = 0.96;
+const kRevealDuration = Duration(milliseconds: 180);
